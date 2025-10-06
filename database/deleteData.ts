aa8e8034f5b9c4b3549dc6e4f12b58ff7e583c01
Instituto AOCP <vitorHI@gmail.com>
2025-10-06 17:38:01 -0300

    save 1

--- database/deleteData.ts
+++ database/deleteData.ts
@@ -1,14 +1,14 @@
 import { prisma } from "./db";
 
 async function deleteData () {
+    console.log("🧹🧹Limpando dados das Tabela campaignLead");
+    await prisma.campaignLead.deleteMany()     
     console.log("🧹🧹Limpando dados das Tabela Campaign");
     await prisma.campaign.deleteMany()   
     console.log("🧹🧹Limpando dados das Tabela Lead");
     await prisma.lead.deleteMany()   
     console.log("🧹🧹Limpando dados das Tabela group");
     await prisma.group.deleteMany()   
-    console.log("🧹🧹Limpando dados das Tabela campaignLead");
- await prisma.campaignLead.deleteMany()     
 }
 
 deleteData()
--- database/seed.ts
+++ database/seed.ts
@@ -5,14 +5,17 @@ import { schemaCreateLead } from "../validation/validation";
 const prisma = new PrismaClient();
 const campaignData = [
   {
+    id: 1,
     name: "Campanha 2025",
     description: "Promoção de inverno focada em produtos de estação.",
   },
   {
+    id: 2,
     name: "Clientes Inativos",
     description: "Estratégia para reengajar.",
   },
   {
+    id: 3,
     name: "Campanha da Empresa",
     description: "Ofertas e comemorações de clientes.",
   },
@@ -20,14 +23,17 @@ const campaignData = [
 
 const groupData = [
   {
+    id: 1,
     name: "Leads Qualificados",
     description: "Leads que demonstraram interesse e foram pré-qualificados.",
   },
   {
+    id: 2,
     name: "Novos Inscritos",
     description: "Leads recém-adquiridos através do site.",
   },
   {
+    id: 3,
     name: "Clientes Recorrentes",
     description: "Leads que já fizeram uma compra no passado.",
   },
@@ -35,30 +41,35 @@ const groupData = [
 
 const leadData = [
   {
+    id: 1,
     name: "Maria Silva",
     email: "maria.silva@exemplo.com",
     phone: 987654321,
     status: "New",
   },
   {
+    id: 2,
     name: "João Pereira",
     email: "joao.pereira@exemplo.com",
     phone: 912345678,
     status: "Contacted",
   },
   {
+    id: 3,
     name: "Ana Souza",
     email: "ana.souza@exemplo.com",
     phone: 998765432,
     status: "Converted",
   },
   {
+    id: 4,
     name: "Carlos Lima",
     email: "carlos.lima@exemplo.com",
     phone: 923456789,
     status: "Unresponsive",
   },
   {
+    id: 5,
     name: "Fernanda Costa",
     email: "fernanda.costa@exemplo.com",
     phone: 934567890,
@@ -96,49 +107,59 @@ const campaignLeadData = [
 
 async function seedData() {
   try {
-    
-//     campaignData.map(async (data) => {
-//       await prisma.campaign.createMany({
-//         data: {
-//           name: data.name,
-//           description: data.description,
-//         },
-//       });
-//   });
-//   console.log("✅Dados da tabela inseridos ✅");
+    for (const data of campaignData) {
+      await prisma.campaign.create({
+        data: {
+          id: data.id,
+          name: data.name,
+          description: data.description,
+        },
+      });
+    }
+    console.log("✅Dados da tabela campaignData inseridos ✅");
 
-// groupData.map(async (data) => {
-//   await prisma.group.createMany({
-//     data: {
-//       name: data.name,
-//       description: data.description,
-//     },
-//   });
-// });
+    for (const data of leadData) {
+      const seedValidationLead = schemaCreateLead.parse(data);
+      await prisma.lead.create({
+        data: {
+          ...seedValidationLead,
+        },
+      });
+    }
+    console.log("✅Dados da tabela leadData inseridos ✅");
 
-
-campaignLeadData.map(async (campaignData) => {
-  const validationCampaignLead = schemaAddCampaignLead.parse(campaignData);
-  
-  await prisma.campaignLead.createMany({
-    data: {
-      campaignId: +campaignData.campaignId,
-      ...validationCampaignLead,
-    },
-  });
-});
-
-// leadData.map(async (datas) =>{
-//   const seedValidationLead = schemaCreateLead.parse(datas)
-//   await prisma.lead.createMany({
-//     data:{
-//       ...seedValidationLead
-//     }
-//   })
-// })
-} catch (error) {
- console.log(error) 
-}
+    for (const data of groupData) {
+      await prisma.group.create({
+        data: {
+          id: data.id,
+          name: data.name,
+          description: data.description,
+        },
+      });
+    }
+    console.log("✅Dados da tabela group inseridos ✅");
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-seedData()
+async function CampaignLeads() {
+  try {
+    for (const data of campaignLeadData) {
+      const query = schemaAddCampaignLead.parse(data);
+      const result = await prisma.campaignLead.create({
+        data: {
+          campaignId: data.campaignId,
+          leadId: query.leadId,
+          status: query.status,
+        },
+      });
+      console.log(`✅Dados da tabela campaignLead inseridos: ${result} ✅`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+seedData();
+CampaignLeads();
